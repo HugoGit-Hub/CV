797cdad11cc25016da7665152bc3350c768ce87b
hugo34130 <82830783+hugo34130@users.noreply.github.com>
2022-03-14 00:53:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+function onSwitch0() {
+    //document.getElementsByTagName('html')[0].style.backgroundImage = 'url(image/CompImg0.jpg)';
+    document.getElementById('skillTwo').style.display = 'none';
+    document.getElementById('skillThree').style.display = 'none';
+    document.getElementById('skillFour').style.display = 'none';
+    document.getElementById('skillFive').style.display = 'none';
+    document.getElementById('skillSix').style.display = 'none';
+    document.getElementById('skillOne').style.display = 'block';
+}
+
+function onSwitch1() {
+    //document.getElementsByTagName('html')[0].style.backgroundImage = 'url(image/CompImg2.2.jpg)';
+    document.getElementById('skillOne').style.display = 'none';
+    document.getElementById('skillThree').style.display = 'none';
+    document.getElementById('skillFour').style.display = 'none';
+    document.getElementById('skillFive').style.display = 'none';
+    document.getElementById('skillSix').style.display = 'none';
+    document.getElementById('skillTwo').style.display = 'block';
+}
+
+function onSwitch2() {
+    //document.getElementsByTagName('html')[0].style.backgroundImage = 'url(image/CompImg2.1.jpeg)';
+    document.getElementById('skillOne').style.display = 'none';
+    document.getElementById('skillTwo').style.display = 'none';
+    document.getElementById('skillFour').style.display = 'none';
+    document.getElementById('skillFive').style.display = 'none';
+    document.getElementById('skillSix').style.display = 'none';
+    document.getElementById('skillThree').style.display = 'block';
+}
+
+function onSwitch3() {
+    //document.getElementsByTagName('html')[0].style.backgroundImage = 'url(image/CompImg3.jpg)';
+    document.getElementById('skillOne').style.display = 'none';
+    document.getElementById('skillTwo').style.display = 'none';
+    document.getElementById('skillThree').style.display = 'none';
+    document.getElementById('skillFive').style.display = 'none';
+    document.getElementById('skillSix').style.display = 'none';
+    document.getElementById('skillFour').style.display = 'block';
+}
+
+function onSwitch4() {
+    //document.getElementsByTagName('html')[0].style.backgroundImage = 'url(image/CompImg4.3.jpg)';
+    document.getElementById('skillOne').style.display = 'none';
+    document.getElementById('skillTwo').style.display = 'none';
+    document.getElementById('skillThree').style.display = 'none';
+    document.getElementById('skillFour').style.display = 'none';
+    document.getElementById('skillSix').style.display = 'none';
+    document.getElementById('skillFive').style.display = 'block';
+}
+
+function onSwitch5() {
+    //document.getElementsByTagName('html')[0].style.backgroundImage = 'url(image/CompImg5.jpg)';
+    document.getElementById('skillOne').style.display = 'none';
+    document.getElementById('skillTwo').style.display = 'none';
+    document.getElementById('skillThree').style.display = 'none';
+    document.getElementById('skillFour').style.display = 'none';
+    document.getElementById('skillFive').style.display = 'none';
+    document.getElementById('skillSix').style.display = 'block';
+}
